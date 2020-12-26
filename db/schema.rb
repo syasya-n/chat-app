59028c330198b2d69284b1b0addeb8fde5b716a9
syasya-n <yousuihiromi@gmail.com>
2020-12-26 12:21:04 +0900

    新規作成画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_26_020746) do
+ActiveRecord::Schema.define(version: 2020_12_26_022922) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
