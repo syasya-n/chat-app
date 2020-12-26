5f159937b3bddd9ff865d76445181b552ec0ad52
syasya-n <yousuihiromi@gmail.com>
2020-12-26 19:36:21 +0900

    メッセージ・チャットルーム名の表示

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_12_26_085554) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "string"
+    t.string "content"
     t.bigint "room_id"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
