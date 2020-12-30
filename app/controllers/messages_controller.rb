65cbe5899f359f06c19472de036e4373694334e2
syasya-n <yousuihiromi@gmail.com>
2020-12-30 09:03:45 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,6 +19,6 @@ class MessagesController < ApplicationController
   private
 
   def message_path
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content,:image).merge(user_id: current_user.id)
   end
 end
